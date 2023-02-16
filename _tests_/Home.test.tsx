d22ab294db88b4074355980442d6a070ff98715f
zia-ur-rehman-warsi <zia.warsi@cooperativecomputing.com>
2023-02-16 20:33:54 +0500

    project setup

---
+++ _tests_/Home.test.tsx
@@ -0,0 +1,15 @@
+import { test } from 'vitest'
+import { render } from '@testing-library/react'
+import Home from '../pages'
+
+test('home', () => {
+  render(<Home />)
+  //   const main = within(screen.getByRole('main'))
+  //   expect(
+  //     main.getByRole('heading', { level: 1, name: /welcome to next\.js!/i }),
+  //   ).toBeDefined()
+
+  //   const footer = within(screen.getByRole('contentinfo'))
+  //   const link = within(footer.getByRole('link'))
+  //   expect(link.getByRole('img', { name: /vercel logo/i })).toBeDefined()
+})
